9dee4b3bc0c0956a7245bce078769bc4d0a8fbd3
JRandy77 <jarandolph77@gmail.com>
2024-06-04 18:19:16 +0000

    bids_split update

--- utils/bids_split.py
+++ utils/bids_split.py
@@ -1,26 +1,22 @@
 import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain_text_splitters import MarkdownTextSplitter
 
 class BidsSplitter:
 
     def __init__(self,
                  src = "./bids-specification/src",
-                 headers_to_split_on = [
-                     ("#", "Header 1"),
-                     ("##", "Header 2"),
-                     ("###", "Header 3"),
-                 ],
+
                  ignore = [],
                  chunk_size = 300,
                  chunk_overlap = 15,
                  ):
         self.src = src
-        self.headers_to_split_on = headers_to_split_on
-        self.md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on = headers_to_split_on)
+        self.md_splitter = MarkdownTextSplitter()
         md_splits = self.crawl_specification(src,ignore)
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap)
-        self.splits = self.text_splitter.split_documents(md_splits)
+        self.splits = self.md_splitter.create_documents(md_splits)
+  #      self.text_splitter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap)
+  #      self.splits = self.text_splitter.split_documents(md_splits)
 
 
 
@@ -41,5 +37,4 @@ class BidsSplitter:
     def get_splits(self):
         return self.splits
 
-    
     
