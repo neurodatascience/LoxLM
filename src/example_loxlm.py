904d27b1e9ad5227268f846015841e646ba4860b
JRandy77 <71611979+JRandy77@users.noreply.github.com>
2024-11-08 16:38:46 -0500

    Update example_loxlm.py

--- src/example_loxlm.py
+++ src/example_loxlm.py
@@ -7,3 +7,14 @@ from loxlm import LoxLM
 loxlm = LoxLM(test=True, log_prompts=True)
 
 loxlm.batch_input()
+"""
+#If you want to perform inference on you own dataset.
+input_dicoms = "replace with path to your DICOMs"
+output_file = "replace with your desired output file"
+
+loxlm = LoxLM(test=False,input_dicoms = input_dicoms, output_file=output_file openai=True)
+loxlm.batch_input()
+#or you could do
+loxlm = LoxLM(test=False, openai=True)
+loxlm.batch_input(input_dicoms=input_dicoms, output_file=output_file)
+"""
